9253f1bd69c431012077a4fe495e13c3738c2ed8
paul <15009257+phansel@users.noreply.github.com>
2018-04-16 00:46:04 -0600

    y

--- p6/src/main.rs
+++ p6/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 0;
+    loop {
+        println!("y");
+        x = x + 1;
+        if x > 15_000_000 {
+            break;
+        }
+    }
 }
