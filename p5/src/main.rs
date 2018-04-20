1228763c6a4bedd39049e1d3ca3d152c31b5488c
paul <15009257+phansel@user.noreply.github.com>
2018-04-20 13:56:04 -0600

    p5f2

--- p5/src/main.rs
+++ p5/src/main.rs
@@ -17,7 +17,6 @@ fn main() {
 
         let p = P {_x: _a, _y: _b, _z: _c, x: a, y: b, z: c, c1: (0,0,0,0) };
 
-
         m.push(p);
         println!("ind: {} _x: {} _y: {} z: {} x: {} y: {} z: {}",
             n, _a, _b, _c, a, b, c);
@@ -33,6 +32,7 @@ fn main() {
         let counts = rand::thread_rng().gen_range(0,256);
         let pt: (f32, f32) = orbit(100.0,0.33,*p);
         println!("x: {} y: {} count: {}", pt.0, pt.1, counts);
+        let pos: (i32, i32, i32) = fnn3d();
     }
 }
 
@@ -50,4 +50,31 @@ fn orbit(c: f32, e: f32, phi: f32) -> (f32, f32) {
     let x: f32 = c*phi.cos() / ( 1.0 + e * phi.cos() );
     let y: f32 = c*phi.sin() / ( 1.0 + e * phi.cos() );
     return (x,y);
+}
+// doesn't work
+fn fnn3d(x: f32, y: f32, z: f32, xr: &[f32],
+    yr: &[f32], zr: &[f32]) -> (i32, i32, i32) {
+
+    let c: (f32, f32, f32) = (x,y,z);
+    for i in 1..3 {
+
+    }
+    return (ix, iy, iz);
+}
+// probably doesn't work
+fn fnn(x: f32, a: &[f32]) -> i32 {
+    if a.len() > 1 {
+        let l: i32 = a.len();
+        let m: i32 = l/2;
+        if a[m] >= x {
+            let pos = fnn(x, a[0..m]);
+        }
+        else {
+            let pos = fnn(x, a[m..l-1]) + m;
+        }
+        return pos;
+    }
+    else {
+        return 0;
+    }
 }
