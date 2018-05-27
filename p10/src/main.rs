c05f8aa558d10612a8076f4c6462fb176436a5b5
paul <15009257+phansel@user.noreply.github.com>
2018-05-26 23:16:32 -0600

    p10_1

--- p10/src/main.rs
+++ p10/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut l: Vec<L> = Vec::with_capacity(19);
     for i in 0..18 {
+        let _a = 'a';
         let _rs = ('0','0','0','0','0','0');
         let _ss = ('0','0','0','0','0','0');
-        let _l = L { x: i, a:'a', rs: _rs, ss: _ss};
+        let _l = L { x: i, a: _a, rs: _rs, ss: _ss};
         l.push(_l);
     }
 }
@@ -13,4 +14,26 @@ struct L {
     a: char,
     rs: (char, char, char, char, char, char),
     ss: (char, char, char, char, char, char),
+}
+
+fn char_to_n (c: char) -> i32 {
+    match c {
+        'w' => 1,
+        'r' => 2,
+        'g' => 3,
+        'b' => 4,
+        's' => 5,
+        _ => 0,
+    }
+}
+
+fn n_to_char (n: i32) -> char {
+    match n {
+        1 => 'w',
+        2 => 'r',
+        3 => 'g',
+        4 => 'b',
+        5 => 's',
+        _ => 'u',
+    }
 }
